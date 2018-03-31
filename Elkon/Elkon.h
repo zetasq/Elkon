3b8b0c1a0b6f2100e57a98f9dd0c548a215092f8
Zhu Shengqi <zetasq@gmail.com>
2018-03-31 20:03:50 +0800

    Initial Commit

---
+++ Elkon/Elkon.h
@@ -0,0 +1,19 @@
+//
+//  Elkon.h
+//  Elkon
+//
+//  Created by Zhu Shengqi on 31/03/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Elkon.
+FOUNDATION_EXPORT double ElkonVersionNumber;
+
+//! Project version string for Elkon.
+FOUNDATION_EXPORT const unsigned char ElkonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elkon/PublicHeader.h>
+
+
